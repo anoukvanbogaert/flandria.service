6c00648b545f8211ecf8a761baed8495cbfc37e9
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2023-01-15 13:00:20 -0800

    putting in schema

---
+++ server-api/db/schema/04_services.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS services CASCADE;
+CREATE TABLE services (
+  id SERIAL PRIMARY KEY NOT NULL,
+  overview_id INTEGER REFERENCES overview(id),
+  boat_id INTEGER REFERENCES boats(id),
+  date TIMESTAMP,
+  remark VARCHAR(255)
+);
