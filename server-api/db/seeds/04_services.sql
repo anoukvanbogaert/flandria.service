76904c38634d7ced3fcb2713c22d6004e0982e7e
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2023-01-15 16:32:01 -0800

    added testing seeds

---
+++ server-api/db/seeds/04_services.sql
@@ -0,0 +1,6 @@
+INSERT INTO services (overview_id, boat_id, date, remark)
+VALUES
+(1, 1, '2022-09-29', 'executed by Guy'),
+(1, 2, '2022-09-29', 'executed by David'),
+(2, 1, '2022-09-29', 'executed by Guy'),
+(2, 2, '2022-09-29', 'executed by David');
