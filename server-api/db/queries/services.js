26b093604e4ece49ddf199089a13ef85970ccb61
anoukvanbogaert <vb.anouk@gmail.com>
2024-04-18 14:04:34 -0700

    replacing old repo

--- server-api/db/queries/services.js
+++ server-api/db/queries/services.js
@@ -6,7 +6,8 @@ const getServices = () => {
     .query(`SELECT services.*, boats.boat_name, overview.service_name as service_name 
     FROM services
     INNER JOIN boats ON services.boat_id=boats.id
-    INNER JOIN overview ON services.overview_id=overview.id`)
+    INNER JOIN overview ON services.overview_id=overview.id
+    ORDER BY date DESC`)
     .then(services => {
       return services.rows;
     })
