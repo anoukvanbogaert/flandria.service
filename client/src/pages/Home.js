7a1f890dc187d86c5cfc5f04ca63991ce2547202
anoukvanbogaert <vb.anouk@gmail.com>
2024-08-14 08:40:08 -0700

    layout title change

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import OverviewList from '../components/clientView/OverviewList';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import './Home.scss';
-import { useStoreState } from 'pullstate';
-import { AppStore } from '../stores/AppStore';
 
 const Home = () => {
     return (
         <Box className='home__container'>
-            <h1 className='home_title'>Services Overview</h1>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                <Typography
+                    variant='h4'
+                    sx={{
+                        textAlign: 'center',
+                        width: '100%',
+                        color: '#045174',
+                        fontWeight: 'bold',
+                        margin: '1rem',
+                    }}
+                >
+                    Services Overview
+                </Typography>
+            </Box>
             <OverviewList />
         </Box>
     );
